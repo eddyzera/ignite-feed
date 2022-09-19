d51e9abb076e269208cd9f2ad856f543b10b44cf
eddyzera <edgar.nascimento@newgo.com.br>
2022-09-19 18:13:28 -0300

    [feat]: use component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import IgniteLogo from '../assets/logo.svg'
 import styles from './Header.module.css'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={IgniteLogo} alt="logo do ignite feed" />
     </header>
   )
 }
