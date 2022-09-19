eccf70dbf1f312bd73351470c3d3ca20ed0bbf00
eddyzera <edgar.nascimento@newgo.com.br>
2022-09-19 18:23:55 -0300

    [feat]: use wrapper for divide screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { Header } from "./components/Header"
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
   return (
-    <Header />
+   <>
+     <Header />
+     <div className={styles.wrapper}>
+      <aside>
+        sidebar
+      </aside>
+      <main>
+        <div>
+          <h1>Edgar Silva</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div>
+          <h1>Edgar Silva</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </main>
+     </div>
+   </>
   )
 }
 
