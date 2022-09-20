73e0e6b02d8c719b1fceb6d23832c0e5605e4436
eddyzera <edgar.nascimento@newgo.com.br>
2022-09-20 00:17:32 -0300

    [feat]: created area for comment

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -30,6 +30,16 @@ export const Post = () => {
           <a href="#">#rocketseat</a>{' '}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder="Deixe um comentario"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
